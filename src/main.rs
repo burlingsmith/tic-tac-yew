0f02533d100e9f9597fd194027d1cf9abc014adf
cmarcbs7 <cmarbs7@gmail.com>
2019-08-02 18:03:21 -0500

    Wrote test_winner()

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ pub enum Player {
 
 #[derive(Debug)]
 pub struct Board {
-    values: [[Option<Player>; 3]; 3]
+    values: [[Option<Player>; 3]; 3],
 }
 
 impl Board {
@@ -34,6 +34,16 @@ impl Board {
         } else {
             None
         }
+    }
+
+    pub fn from_array(values: [[Option<Player>; 3]; 3]) -> Self {
+        Self {
+            values: values,
+        }
+    }
+
+    pub fn winner(&self) -> Option<Player> {
+        unimplemented!();
     }
 }
 
@@ -60,12 +70,83 @@ mod tests {
 
         for col in 0..3 {
             for row in 0..3 {
-                assert_eq!(
-                    new_board.get((col, row)),
-                    None,
-                    "found 'Some(_)' in instantiated board; expected 'None'"
-                );
+                assert_eq!(new_board.get((col, row)), None);
             }
         }
     }
+
+    #[test]
+    fn test_winner() {
+        let horizontal_x_win_1 = Board::from_array(
+            [
+                [Some(Player::X), Some(Player::X), Some(Player::X)],
+                [None, None, None],
+                [None, None, None],
+           ]
+        );
+        let horizontal_o_win_2 = Board::from_array(
+            [
+                [None, None, None],
+                [Some(Player::O), Some(Player::O), Some(Player::O)],
+                [None, None, None],
+           ]
+        );
+        let horizontal_x_win_3 = Board::from_array(
+            [
+                [None, None, None],
+                [None, None, None],
+                [Some(Player::X), Some(Player::X), Some(Player::X)],
+           ]
+        );
+
+        assert_eq!(horizontal_x_win_1.winner(), Some(Player::X));
+        assert_eq!(horizontal_o_win_2.winner(), Some(Player::O));
+        assert_eq!(horizontal_x_win_3.winner(), Some(Player::X));
+
+        let vertical_o_win_1 = Board::from_array(
+            [
+                [Some(Player::O), None, None],
+                [Some(Player::O), None, None],
+                [Some(Player::O), None, None],
+            ]
+        );
+        let vertical_x_win_2 = Board::from_array(
+            [
+                [None, Some(Player::X), None],
+                [None, Some(Player::X), None],
+                [None, Some(Player::X), None],
+            ]
+        );
+        let vertical_o_win_3 = Board::from_array(
+            [
+                [None, None, Some(Player::O)],
+                [None, None, Some(Player::O)],
+                [None, None, Some(Player::O)],
+            ]
+        );
+
+        assert_eq!(vertical_o_win_1.winner(), Some(Player::O));
+        assert_eq!(vertical_x_win_2.winner(), Some(Player::X));
+        assert_eq!(vertical_o_win_3.winner(), Some(Player::O));
+
+        let diagonal_x_win_1 = Board::from_array(
+            [
+                [Some(Player::X), None, None],
+                [None, Some(Player::X), None],
+                [None, None, Some(Player::X)],
+            ]
+        );
+        let diagonal_o_win_2 = Board::from_array(
+            [
+                [None, None, Some(Player::O)],
+                [None, Some(Player::O), None],
+                [Some(Player::O), None, None],
+            ]
+        );
+
+        assert_eq!(diagonal_x_win_1.winner(), Some(Player::X));
+        assert_eq!(diagonal_o_win_2.winner(), Some(Player::O));
+    }
+
+    // test_no_winner corners, center, edges
 }
