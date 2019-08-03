ac7fda8121920985edad1dc8cbe3d9da62783d81
cmarcbs7 <cmarbs7@gmail.com>
2019-08-02 19:12:02 -0500

    Passing test_winner()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 //! Tic-tac-toe written in Rust using the Yew framework.
 
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
 //////////////////////////////////////////////////////////////////////////////
 // Convenience Aliases
 //////////////////////////////////////////////////////////////////////////////
@@ -10,7 +13,7 @@ pub type Index = (usize, usize);
 //
 //////////////////////////////////////////////////////////////////////////////
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum Player {
     X,
     O,
@@ -22,17 +25,13 @@ pub struct Board {
 }
 
 impl Board {
+    //////////////////////////////////
+    // Instantiation
+    //////////////////////////////////
+
     pub fn new() -> Self {
         Self {
             values: [[None; 3]; 3],
-        }
-    }
-
-    pub fn get(&self, (col, row): Index) -> Option<Player> {
-        if col < 3 && row < 3 {
-            self.values[col][row]
-        } else {
-            None
         }
     }
 
@@ -42,8 +41,81 @@ impl Board {
         }
     }
 
+    //////////////////////////////////
+    // Access
+    //////////////////////////////////
+
+    pub fn get(&self, (col, row): Index) -> Option<Player> {
+        if col < 3 && row < 3 {
+            self.values[col][row]
+        } else {
+            None
+        }
+    }
+
+    //////////////////////////////////
+    // Evaluation
+    //////////////////////////////////
+
     pub fn winner(&self) -> Option<Player> {
-        unimplemented!();
+        let mut result = None;
+
+        let only_has = |set: &HashSet<Option<Player>>, player| {
+            set.len() == 1 && set.contains(&Some(player))
+        };
+
+        // Vertical checks
+        for col in 0..3 {
+            let column: HashSet<Option<Player>> = {
+                HashSet::from_iter(self.values[col].iter().cloned())
+            };
+
+            if only_has(&column, Player::X) {
+                result = Some(Player::X);
+                break;
+            } else if only_has(&column, Player::O) {
+                result = Some(Player::O);
+                break;
+            }
+        }
+
+        // Horizontal checks
+        if result.is_none() {
+            'outer: for row in 0..3 {
+                let mut row_vals: HashSet<Option<Player>> = HashSet::new();
+
+                for col in 0..3 {
+                    row_vals.insert(self.values[col][row]);
+                }
+
+                if only_has(&row_vals, Player::X) {
+                    result = Some(Player::X);
+                    break 'outer;
+                } else if only_has(&row_vals, Player::O) {
+                    result = Some(Player::O);
+                    break 'outer;
+                }
+            }
+        }
+
+        // Diagonal checks
+        if result.is_none() {
+            let mut s1: HashSet<Option<Player>> = HashSet::new();
+            let mut s2: HashSet<Option<Player>> = HashSet::new();
+
+            for n in 0..3 {
+                s1.insert(self.values[n][n]);
+                s2.insert(self.values[n][2 - n]);
+            }
+
+            if only_has(&s1, Player::X) || only_has(&s2, Player::X) {
+                result = Some(Player::X);
+            } else if only_has(&s1, Player::O) || only_has(&s2, Player::O) {
+                result = Some(Player::O);
+            }
+        }
+
+        result
     }
 }
 
@@ -52,7 +124,16 @@ impl Board {
 //////////////////////////////////////////////////////////////////////////////
 
 fn main() {
-    println!("Hello, world!");
+    let mut value = "Heyo!";
+
+    for i in 0..3 {
+        if i == 1 {
+            value = "Hello, World!";
+            break;
+        }
+    }
+
+    println!("{}", value);
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -148,5 +229,9 @@ mod tests {
         assert_eq!(diagonal_o_win_2.winner(), Some(Player::O));
     }
 
-    // test_no_winner corners, center, edges
+    // test_no_winner corners, center, edges; mixed (e.g. xox)
+
+    // test setter
+
+    // test draw
 }
